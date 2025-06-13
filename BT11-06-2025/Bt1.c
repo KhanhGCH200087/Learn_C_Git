9490073c669fa312b49b0177b5959d7ffe843f7b
KhanhGCH200087 <105761671+KhanhGCH200087@users.noreply.github.com>
2025-06-13 18:24:04 +0700

    Update Bt1.c

--- BT11-06-2025/Bt1.c
+++ BT11-06-2025/Bt1.c
@@ -5,9 +5,9 @@ int main(void){
     int temp;
     printf("Enter temperature: ");
     scanf("%d", &temp);
-    if(10 <= temp <= 25){
+    if(temp >= 10 && temp <= 25){
         printf("\nLed level 1");
-    } else if (26 <= temp <= 30){
+    } else if (temp >= 26 && temp <= 30){
         printf("\nLed level 2");
     } else {
         printf("\n Led level 0");
